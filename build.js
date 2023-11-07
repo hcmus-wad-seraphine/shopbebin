78ea05bcd66028b80f7c9e9b1ffb4bb519b6912b
dev.ngtronghieu <dev.ngtronghieu@gmail.com>
2023-11-07 08:39:29 +0700

    refactor: clean up folder structure

--- build.js
+++ build.js
@@ -1,8 +1,6 @@
 const esbuild = require("esbuild");
-const fs = require("fs");
 const path = require("path");
 
-const viewsDir = path.resolve(__dirname, "./views");
 const buildDir = path.resolve(__dirname, "./build");
 
 esbuild
@@ -16,32 +14,3 @@ esbuild
         platform: "node",
     })
     .catch(() => process.exit(1));
-
-fs.readdir(viewsDir, (err, files) => {
-    if (err) {
-        console.error("Error reading views directory:", err);
-        process.exit(1);
-    }
-
-    const cssFiles = files.filter((file) => file.endsWith(".css"));
-
-    cssFiles.forEach((file) => {
-        const entryPoint = path.join(viewsDir, file);
-        const outFile = path.join(buildDir, file);
-
-        esbuild
-            .build({
-                entryPoints: [entryPoint],
-                outfile: outFile,
-                bundle: true,
-                minify: true,
-                sourcemap: true,
-                target: ["es2020"],
-                platform: "browser",
-            })
-            .catch(() => {
-                console.error(`Error building ${file}`);
-                process.exit(1);
-            });
-    });
-});
