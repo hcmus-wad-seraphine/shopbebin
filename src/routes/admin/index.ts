41353aa00180c8e874a7c8ddbb37d639ea685dac
dev.ngtronghieu <dev.ngtronghieu@gmail.com>
2023-11-13 23:55:48 +0700

    feat: mock admin pages

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -6,4 +6,20 @@ router.get("/", (req, res) => {
     res.render("admin");
 });
 
+router.get("/profile", (req, res) => {
+    res.render("admin/Profile");
+});
+
+router.get("/accounts", (req, res) => {
+    res.render("admin/Accounts");
+});
+
+router.get("/vendors", (req, res) => {
+    res.render("admin/Vendors");
+});
+
+router.get("/products", (req, res) => {
+    res.render("admin/Products");
+});
+
 export default router;
