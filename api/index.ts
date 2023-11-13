b30e34cb19a96fdb4dbc09f380a9af54b64a88a7
dev.ngtronghieu <dev.ngtronghieu@gmail.com>
2023-11-13 22:45:59 +0700

    chore: test vercel build

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,33 @@
-import app from "../src/app";
+import "dotenv/config";
+
+import express from "express";
+
+import compression from "compression";
+
+import RootRouter from "../src/routes";
+import CustomerLoginRouter from "../src/routes/login";
+import CustomerRegisterRouter from "../src/routes/register";
+import CustomerCartRouter from "../src/routes/cart";
+import CustomerCheckoutRouter from "../src/routes/checkout";
+
+import AdminRouter from "../src/routes/admin";
+import AdminLoginRouter from "../src/routes/admin/login";
+
+const app = express();
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use(compression());
+app.use(express.static("public"));
+
+app.use("/", RootRouter);
+app.use("/login", CustomerLoginRouter);
+app.use("/register", CustomerRegisterRouter);
+app.use("/cart", CustomerCartRouter);
+app.use("/checkout", CustomerCheckoutRouter);
+
+app.use("/admin", AdminRouter);
+app.use("/admin/login", AdminLoginRouter);
 
 export default app;
