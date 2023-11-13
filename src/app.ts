9b9960d36d8b57d39eacd1942285b23e7dae83ff
dev.ngtronghieu <dev.ngtronghieu@gmail.com>
2023-11-13 22:29:11 +0700

    chore: use static public

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,6 @@ import express from "express";
 
 import compression from "compression";
 
-import { sequelize } from "./models/database/sequelize";
-
 import RootRouter from "./routes";
 import CustomerLoginRouter from "./routes/login";
 import CustomerRegisterRouter from "./routes/register";
@@ -21,7 +19,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(compression());
-app.use(express.static("public"));
+app.use(express.static("/public"));
 
 app.use("/", RootRouter);
 app.use("/login", CustomerLoginRouter);
