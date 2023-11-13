a6e418ee0f79dee0f7735ce4201f0eae909a98da
ltminhthu <ltminhthu.209@gmail.com>
2023-11-13 20:43:05 +0700

    add routers to the app

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import { sequelize } from "./models/database/sequelize";
 
 import RootRouter from "./routes";
 import CustomerLoginRouter from "./routes/login";
+import CustomerRegisterRouter from "./routes/register";
+import CustomerCartRouter from "./routes/cart";
+import CustomerCheckoutRouter from "./routes/checkout";
 
 import AdminRouter from "./routes/admin";
 import AdminLoginRouter from "./routes/admin/login";
@@ -30,6 +33,9 @@ const start = async () => {
 
     app.use("/", RootRouter);
     app.use("/login", CustomerLoginRouter);
+    app.use("/register", CustomerRegisterRouter);
+    app.use("/cart", CustomerCartRouter);
+    app.use("/checkout", CustomerCheckoutRouter);
 
     app.use("/admin", AdminRouter);
     app.use("/admin/login", AdminLoginRouter);
