9a167b1e1d0c8022d0bd046588de17ce42661a8c
dev.ngtronghieu <dev.ngtronghieu@gmail.com>
2023-11-17 01:52:18 +0700

    refactor: rename 'routes' to 'pages'

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import UserRoot from "./routes/UserRoot";
-import AdminRoot from "./routes/AdminRoot";
+import UserRoot from "./pages/UserRoot";
+import AdminRoot from "./pages/AdminRoot";
 
 const router = createBrowserRouter([
     {
---
+++ src/views/pages/AdminRoot.tsx
@@ -0,0 +1,11 @@
+import Container from "../components/Container";
+
+const AdminRoot = () => {
+    return (
+        <Container>
+            <h1>Admin Root</h1>
+        </Container>
+    );
+};
+
+export default AdminRoot;
