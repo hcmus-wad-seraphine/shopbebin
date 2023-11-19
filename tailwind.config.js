52b0da54e9c68c9efb6dffce36ec532d0f480820
ltminhthu <ltminhthu.209@gmail.com>
2023-11-19 21:53:04 +0700

    config tailwind and add icons library

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
     content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': '#7FA0F4',
+                'secondary': '#D1A3FF',
+                'black': '#525252',
+              },
+              fontFamily: {
+                'mono': ['"Poppins"'],
+              },
+        },
     },
     plugins: [],
 };
