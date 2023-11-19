c2ed3b6671e42c5d39c6510abfa43e1d1e6fc12b
ltminhthu <ltminhthu.209@gmail.com>
2023-11-19 21:53:20 +0700

    add routes

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -3,21 +3,36 @@ import { render } from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import UserRoot from "./pages/UserRoot";
 import AdminRoot from "./pages/AdminRoot";
+import LogIn from "./pages/LogIn";
+import Register from "./pages/Register";
+import CartPage from "./pages/Cart";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <UserRoot />,
-    },
-    {
-        path: "/admin",
-        element: <AdminRoot />,
-    },
+  {
+    path: "/",
+    element: <UserRoot />,
+  },
+  {
+    path: "/admin",
+    element: <AdminRoot />,
+  },
+  {
+    path: "/login",
+    element: <LogIn />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
+  },
 ]);
 
 render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
