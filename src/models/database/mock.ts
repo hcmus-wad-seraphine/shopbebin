f21a3157e2ceaa80183f5d1b6cc65589906ca2d7
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-11-22 08:33:42 +0700

    chore: add more mock data

--- src/models/database/mock.ts
+++ src/models/database/mock.ts
@@ -1,4 +1,4 @@
-import { ProductMetadata } from "@prisma/client";
+import { ProductMetadata, ProductSize, Size, User } from "@prisma/client";
 
 export const mockProducts: ProductMetadata[] = [
     {
@@ -24,5 +24,58 @@ export const mockProducts: ProductMetadata[] = [
         name: "Red Thai Bubble",
         desc: "",
         images: ["/img/tea-redthai-bubble.png"],
+    },
+];
+
+export const mockProductSizes: ProductSize[] = [
+    {
+        id: "1",
+        productMetadataId: "smoothie-strawberry-bubble",
+        price: 2,
+        size: Size.S,
+        stock: 10,
+    },
+    {
+        id: "2",
+        productMetadataId: "smoothie-strawberry-bubble",
+        price: 2.2,
+        size: Size.M,
+        stock: 10,
+    },
+    {
+        id: "3",
+        productMetadataId: "smoothie-strawberry-bubble",
+        price: 2.4,
+        size: Size.L,
+        stock: 10,
+    },
+];
+
+export const mockUsers: User[] = [
+    {
+        id: "1",
+        email: "user1@shopbebin.com",
+        phone: "09123456789",
+        addresses: [
+            {
+                unitNumber: "123A",
+                street: "Street 1",
+                district: "District 1",
+                city: "City 1",
+            },
+        ],
+    },
+    {
+        id: "2",
+        email: "user2@shopbebin.com",
+        phone: "09123456789",
+        addresses: [
+            {
+                unitNumber: "123B",
+                street: "Street 2",
+                district: "District 2",
+                city: "City 2",
+            },
+        ],
     },
 ];
