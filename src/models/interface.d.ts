77ef74d9fc7563fd5580285d6ee51a615d974cda
dev.ngtronghieu <dev.ngtronghieu@gmail.com>
2023-11-26 22:35:59 +0700

    refactor: update query

--- src/models/interface.d.ts
+++ src/models/interface.d.ts
@@ -3,10 +3,15 @@ import {
   type ProductMetadata,
   type ProductSize,
   type ProductTopping,
+  type ToppingMetadata,
 } from "@prisma/client";
+
+export type Topping = ProductTopping & {
+  topping: ToppingMetadata;
+};
 
 export type Product = ProductMetadata & {
   availableSizes: ProductSize[];
-  availableToppings: ProductTopping[];
+  availableToppings: Topping[];
   category: Category;
 };
