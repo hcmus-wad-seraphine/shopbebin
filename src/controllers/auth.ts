325fcd21022db35541085b18eba0868f98c578b5
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-11-27 13:00:00 +0700

    feat: login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import { type RequestHandler } from "express";
 import { type ErrorResponse } from "react-router-dom";
 
-import { createUser } from "../models/users";
+import { createUser, getUserByEmail, getUserByPhone } from "../models/users";
 
 export const register: RequestHandler = (req, res) => {
   const { email, phone, password } = req.body;
@@ -29,6 +29,56 @@ export const register: RequestHandler = (req, res) => {
       const errorResponse: ErrorResponse = {
         status: 500,
         statusText: "Email or phone already exists",
+        data: err,
+      };
+
+      res.status(500).json(errorResponse);
+    });
+};
+
+export const login: RequestHandler = (req, res) => {
+  const { identifier, password } = req.body;
+  const isEmail = identifier.includes("@");
+
+  console.log("--> login", identifier, password);
+
+  const getUser = async () => {
+    if (isEmail === true) {
+      return await getUserByEmail(identifier);
+    } else {
+      return await getUserByPhone(identifier);
+    }
+  };
+
+  getUser()
+    .then((user) => {
+      if (user === null) {
+        const errorResponse: ErrorResponse = {
+          status: 401,
+          statusText: "User not found",
+          data: null,
+        };
+        return res.status(401).json(errorResponse);
+      }
+
+      const passwordMatch = bcrypt.compareSync(password, user.passwordHash);
+
+      if (!passwordMatch) {
+        const errorResponse: ErrorResponse = {
+          status: 401,
+          statusText: "Incorrect password",
+          data: null,
+        };
+        return res.status(401).json(errorResponse);
+      }
+
+      const { passwordHash, ...userWithoutPasswordHash } = user;
+      res.status(200).json(userWithoutPasswordHash);
+    })
+    .catch((err) => {
+      const errorResponse: ErrorResponse = {
+        status: 500,
+        statusText: "Unknown error occurred",
         data: err,
       };
 
