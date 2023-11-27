b16e1f68d1acb11631083fc62b3f9661a3aa2bd3
ltminhthu <ltminhthu.209@gmail.com>
2023-11-27 14:00:36 +0700

    new utils bar

---
+++ src/views/layouts/UtilsBar.tsx
@@ -0,0 +1,15 @@
+const UtilsBar = () => {
+  return (
+    <div className="items-center justify-between py-3 px-10">
+      <div className="gap-6">
+        <a>Milk tea</a>
+        <a>Fruit tea</a>
+        <a>Smoothie</a>
+        <a>Ice cream</a>
+        <a>Snack</a>
+      </div>
+    </div>
+  );
+};
+
+export default UtilsBar;
