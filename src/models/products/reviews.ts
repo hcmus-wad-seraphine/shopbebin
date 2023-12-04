7b8cb2fa90646bd6f207cd60eb24cfb01210fde8
ltminhthu <ltminhthu.209@gmail.com>
2023-12-04 22:04:14 +0700

    create schema for reviews

---
+++ src/models/products/reviews.ts
@@ -0,0 +1,84 @@
+import { type ProductReview, type ReviewMetadata } from "@prisma/client";
+
+import { getPrismaClient } from "../prisma";
+
+export const createReview = async (review: ReviewMetadata) => {
+  const client = getPrismaClient();
+
+  const { id, ...data } = review;
+
+  const createdReview = await client.reviewMetadata.create({
+    data,
+  });
+
+  return createdReview;
+};
+
+export const createProductReview = async (productReview: ProductReview) => {
+  const client = getPrismaClient();
+  const createdProductReview = await client.productReview.create({
+    data: {
+      productMetadataId: productReview.productMetadataId,
+      reviewMetadataId: productReview.reviewMetadataId,
+    },
+    include: {
+      review: true,
+    },
+  });
+
+  return createdProductReview;
+};
+
+export const getProductReview = async (id: string) => {
+  const client = getPrismaClient();
+
+  return await client.productReview.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      review: true,
+    },
+  });
+};
+
+export const getReviews = async () => {
+  const client = getPrismaClient();
+
+  return await client.reviewMetadata.findMany();
+};
+
+export const getReview = async (id: string) => {
+  const client = getPrismaClient();
+
+  return await client.reviewMetadata.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const updateReview = async (review: ReviewMetadata) => {
+  const client = getPrismaClient();
+
+  const { id, ...data } = review;
+
+  const updatedReview = await client.reviewMetadata.update({
+    where: {
+      id,
+    },
+    data,
+  });
+
+  return updatedReview;
+};
+
+export const deleteReview = async (id: string) => {
+  const client = getPrismaClient();
+
+  return await client.reviewMetadata.delete({
+    where: {
+      id,
+    },
+  });
+};
