e00b83c3347e32d3fd996b36a386a5c1011c2b40
ltminhthu <ltminhthu.209@gmail.com>
2023-12-04 16:01:05 +0700

    change layout of product info

--- src/views/pages/ProductDetails/ProductInfo.tsx
+++ src/views/pages/ProductDetails/ProductInfo.tsx
@@ -10,13 +10,13 @@ const ProductInfo = ({ name, desc, price }: ProductInfoProps) => {
   return (
     <div className="flex-col font-primary">
       <h1 className="leading-relaxed font-extrabold text-3xl text-primary py-2">{name}</h1>
-      <p className="font-medium text-lg">{desc}</p>
       <div className="text-xl text-black font-medium py-2 px-1">
         <Price
           num={price}
           numSize="text-2xl"
         />
       </div>
+      <p className="font-medium text-lg py-2">{desc}</p>
     </div>
   );
 };
