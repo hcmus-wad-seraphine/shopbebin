6260bc011207b9c85b32c8fb32f01726de7a6a55
ltminhthu <ltminhthu.209@gmail.com>
2023-12-31 14:32:03 +0700

    change information

--- src/views/pages/Profile.tsx
+++ src/views/pages/Profile.tsx
@@ -1,10 +1,13 @@
+import { type User } from "@prisma/client";
 import InfoItem from "@views/components/Checkout/InfoItem";
 import Dropdown from "@views/components/Dropdown";
+import ChangePassword from "@views/features/Profile/ChangePassword";
 import InformationTitle from "@views/features/Profile/Information";
 import SettingTitle, { SettingItem } from "@views/features/Profile/Setting";
-import { appActions } from "@views/valtio";
+import { appActions, appState } from "@views/valtio";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSnapshot } from "valtio";
 
 const Profile = () => {
   const [isEditMode, setIsEditMode] = useState(false);
@@ -15,24 +18,27 @@ const Profile = () => {
     navigate("/login");
   };
 
+  const { profile } = useSnapshot(appState);
+  const { email, phone } = profile?.user as User;
+
   const InformationItems = [
     <InfoItem
       key="1"
       isDisabled={!isEditMode}
       title="Name"
-      value="John Doe"
+      value={email}
     />,
     <InfoItem
       key="2"
       isDisabled={!isEditMode}
       title="Phone number"
-      value="0779944549"
+      value={phone}
     />,
     <InfoItem
       key="3"
       isDisabled={!isEditMode}
       title="Email"
-      value="bindum@gmail.com"
+      value={email}
     />,
   ];
 
@@ -41,6 +47,7 @@ const Profile = () => {
       key="1"
       title="Change password"
       icon="key"
+      component={<ChangePassword />}
     />,
     <SettingItem
       key="1"
