80c6b5b39f4f5aced9100d5bebf23282ff8f7586
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-01-03 13:38:47 +0700

    chore: add chartjs

--- src/views/pages/Admin/Dashboard.tsx
+++ src/views/pages/Admin/Dashboard.tsx
@@ -1,16 +1,49 @@
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { useState } from "react";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+enum Period {
+  Daily = "Daily",
+  Weekly = "Weekly",
+  Yearly = "Yearly",
+}
+
 const AdminDashboard = () => {
+  const [period, setPeriod] = useState(Period.Daily);
+
   return (
     <div className="w-full gap-10">
       <div className="flex-col w-[60%]">
         <h1>Report revenue</h1>
 
-        <select className="self-start">
-          <option>Daily</option>
-          <option>Weekly</option>
-          <option>Yearly</option>
+        <select
+          className="self-start"
+          value={period}
+          onChange={(e) => {
+            setPeriod(e.target.value as Period);
+          }}
+        >
+          <option>{Period.Daily}</option>
+          <option>{Period.Weekly}</option>
+          <option>{Period.Yearly}</option>
         </select>
 
-        <div></div>
+        {/* <Bar
+          data={{
+            datasets: [],
+          }}
+          options={}
+        /> */}
       </div>
 
       <div className="flex-col w-[40%]">
