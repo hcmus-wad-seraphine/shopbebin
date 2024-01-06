38ac7f0b790ae5fa7d5b66080d4d08ef421d0339
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-01-06 17:44:05 +0700

    feat: add pagination for admin products

--- src/views/pages/Admin/Products.tsx
+++ src/views/pages/Admin/Products.tsx
@@ -1,33 +1,43 @@
 import { type ShopbebinProduct } from "@models/interface";
+import Pagination from "@views/components/Pagination";
+import Title from "@views/components/Title";
 import ProductRow from "@views/features/Products/ProductRow";
 import ProductTitleRow from "@views/features/Products/ProductTitleRow";
-import { appActions, appState } from "@views/valtio";
+import { appState } from "@views/valtio";
 import { useEffect, useState } from "react";
-import { useSnapshot } from "valtio";
 
 const ProductsPage = () => {
   const [products, setProducts] = useState<ShopbebinProduct[]>([]);
-  const [totalProducts, setTotalProducts] = useState<number>(0);
-  const { queryString } = useSnapshot(appState);
+  const [total, setTotal] = useState<number>(0);
+  const [offset, setOffset] = useState(0);
+
+  const limit = 10;
+  const totalPages = Math.ceil(total / limit);
+  const currentPage = Math.floor(offset / limit) + 1;
 
   useEffect(() => {
     const fetchData = async () => {
-      const endpoint = "/api/products" + appActions.getQueryStrings();
-
-      const productsResponse = await fetch(endpoint);
+      const endpoint = `/api/products?offset=${offset}&limit=${limit}`;
+      const productsResponse = await fetch(endpoint, {
+        headers: {
+          Authorization: `Bearer ${appState.profile?.token}`,
+        },
+      });
       const data = await productsResponse.json();
 
       setProducts(data.products);
-      setTotalProducts(data.total);
+      setTotal(data.total);
     };
 
     fetchData().catch((err) => {
       console.log(err);
     });
-  }, [queryString]);
+  }, [limit, offset]);
 
   return (
     <div className="flex-col gap-2 w-full">
+      <Title text="Products" />
+
       <ProductTitleRow />
 
       {products.map((product) => (
@@ -36,6 +46,15 @@ const ProductsPage = () => {
           product={product}
         />
       ))}
+
+      <Pagination
+        currentPage={currentPage}
+        limit={limit}
+        total={totalPages}
+        onGoToPage={(page) => {
+          setOffset((page - 1) * limit);
+        }}
+      />
     </div>
   );
 };
